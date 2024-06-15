97998e1428de6d5bb234920594374ba78866dcc5
Koval Bohdan <bohdankoval3012@gmail.com>
2024-06-15 11:37:44 +0300

    Realise working modal window

--- js/script.js
+++ js/script.js
@@ -101,4 +101,38 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadLine);
+
+    //Modal
+
+    const modelTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modelCloseBtn = document.querySelector('[data-close]');
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modelTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // modal.classList.toggle('show');
+            document.body.style.overflow = "hidden";
+        });
+    });
+
+    modelCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal){
+            closeModal();
+        }
+    });
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')){
+            closeModal();
+        }
+    });
 });
