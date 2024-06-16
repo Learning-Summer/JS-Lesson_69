0bc6d2a4604775ef42e641821ce4ee73d9beb2d5
Koval Bohdan <bohdankoval3012@gmail.com>
2024-06-16 16:53:55 +0300

    Use rest classes for default class html page

--- js/script.js
+++ js/script.js
@@ -153,12 +153,13 @@ window.addEventListener('DOMContentLoaded', () => {
     // Использование классов для карточок
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
 
             this.transfer = 27;
@@ -171,8 +172,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item'
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -181,7 +188,6 @@ window.addEventListener('DOMContentLoaded', () => {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
             `;
             this.parent.append(element)
         }
@@ -194,7 +200,7 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Премиум"',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         550,
-        '.menu .container'
+        '.menu .container',
     ).render();
 
     new MenuCard(
@@ -203,7 +209,9 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Eко"',
         'В меню “Еко” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         350,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
+        'big',
     ).render();
 
     new MenuCard(
@@ -212,6 +220,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное" ',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         730,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
+        'big',
     ).render();
 });
